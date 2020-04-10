59f182b98861ba5760edd811d2ec3271d357a418
vjguzman <vjguzman@miuandes.cl>
2020-04-10 19:33:11 -0400

    listoco :)

--- app/models/event_stat.rb
+++ app/models/event_stat.rb
@@ -5,23 +5,26 @@ class EventStat < ApplicationRecord
       stat = self.ticket_type.event.event_stat
       stat.attendance +=1
       stat.tickets_sold +=1
-      stat.save!
+      stat.save
     end
   after_destroy :actualizar_stats_unsold
   private
     def actualizar_stats_unsold
       stat = self.ticket_type.event.event_stat
       stat.attendance -=1
-      stat.ticket_sold -=1
-      stat.save!
+      stat.tickets_sold -=1
+      stat.save
     end
-  before_save :max_capacidad
+  before_create :max_capacidad
   private
    def max_capacidad
      stat = self.ticket_type.event.event_stat
      sold = stat.ticket_sold
      capacity = self.ticket_type.event.event_venue.capacity
-     validates :sold, numericality: {less_than_or_equal_to: capacity}
+     if sold >= capacity
+       puts "ERROR BECAUSE THE TICKETS_SOLD WOULD BE BIGGER THAN THE CAPACITY OF THE VENUE"
+       throw :abort
+     end
    end
   belongs_to :event
 end
